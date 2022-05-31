ec882b78db8e9133a60d6c765838994050f362ae
vladislav-kochetkov <vladislav-kochetkov@exactprosystems.com>
2022-05-31 21:01:43 +0300

    First commit

--- include/zmq/i_transport.h
+++ include/zmq/i_transport.h
@@ -1,3 +1,13 @@
+/*
+    This is free software; you can redistribute and/or modify it under the
+    terms of the GNU Lesser General Licence as published by the Free Software
+    Foundation. See the COPYING file for more information.
+    
+    This software is distributed WITHOUT ANY WARRANTY and without even the
+    implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+
 #ifndef I_TRANSPORT_H_
 #define I_TRANSPORT_H_
 
